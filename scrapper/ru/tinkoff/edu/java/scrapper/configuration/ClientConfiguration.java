b6f116e90d5130f1013a25deaa96bb4ad16c3257
Ilya Kuznetsov <78645533+EuphoriaV@users.noreply.github.com>
2023-03-19 20:51:02 +0300

    task 3

---
+++ scrapper/ru/tinkoff/edu/java/scrapper/configuration/ClientConfiguration.java
@@ -0,0 +1,19 @@
+package ru.tinkoff.edu.java.scrapper.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import ru.tinkoff.edu.java.scrapper.client.GitHubClient;
+import ru.tinkoff.edu.java.scrapper.client.StackOverflowClient;
+
+@Configuration
+public class ClientConfiguration {
+    @Bean
+    public GitHubClient gitHubClient() {
+        return new GitHubClient();
+    }
+
+    @Bean
+    public StackOverflowClient stackOverflowClient() {
+        return new StackOverflowClient();
+    }
+}
