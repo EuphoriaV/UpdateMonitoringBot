252c4bed075e6d97322fc7b92561ff9e07de1c54
Ilya Kuznetsov <78645533+EuphoriaV@users.noreply.github.com>
2023-05-05 15:12:33 +0300

    checkstyle should work

--- scrapper/src/main/java/ru/tinkoff/edu/java/scrapper/ScrapperApplication.java
+++ scrapper/src/main/java/ru/tinkoff/edu/java/scrapper/ScrapperApplication.java
@@ -9,7 +9,7 @@ import ru.tinkoff.edu.java.scrapper.configuration.ApplicationConfig;
 @SpringBootApplication(exclude = LiquibaseAutoConfiguration.class)
 @EnableConfigurationProperties({ApplicationConfig.class})
 public class ScrapperApplication {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         var ctx = SpringApplication.run(ScrapperApplication.class, args);
         ApplicationConfig config = ctx.getBean(ApplicationConfig.class);
         System.out.println(config);
